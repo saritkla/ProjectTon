def08dbdaa9bd6277ed2da998259270a66a01089
59050316 <sarit.28381@gmail.com>
2020-02-18 00:08:21 +0700

    done login

--- app/src/main/java/com/example/goodreader/Login.java
+++ app/src/main/java/com/example/goodreader/Login.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,7 +20,7 @@ public class Login extends AppCompatActivity {
     Button loginbt;
     Button regisbt;
     EditText editname;
-
+    boolean issuccess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +28,23 @@ public class Login extends AppCompatActivity {
         loginbt = (Button) findViewById(R.id.loginBt);
         regisbt = (Button) findViewById(R.id.regisBt);
         editname = (EditText) findViewById(R.id.editname);
+        issuccess = false;
+//        issuccess = validate(editname.getText().toString());
         loginbt.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    CheckUser();
+                    issuccess = validate(editname.getText().toString());
+                    if (issuccess){
+                        Intent goStart = new Intent(Login.this,StartGame.class);
+                        startActivity(goStart);
+                    }
+                    else {
+                        Intent goregis = new Intent(Login.this,regisAc.class);
+                        startActivity(goregis);
+                    }
                 }
-            });
+
+        });
         regisbt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,31 +53,32 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-    private void CheckUser(){
-        String User = editname.getText().toString().trim().toLowerCase();
-        boolean issuccess = false;
+
+
+    public boolean validate(String userName) {
+        String[] user = new String[0];
+        String name;
+        int lang = 0;
+        boolean ch = false;
         try {
             JSONArray jArray = new JSONArray(readJSONFromAsset());
-            String name;
-            for (int i = 0 ;i<=jArray.length();i++){
-                name = jArray.getJSONObject(i).getString("Username");
-                if (name.equals(User)){
-                    issuccess = true;
-                }
+            lang = jArray.length();
+//            user = new String[lang];
+            for (int i = 0 ;i < lang;i++){
+                JSONObject currObject = jArray.getJSONObject(i);
+                name = currObject.getString("Username");
+                if (name.equals(userName)) ch = true;
+//                user[i] = name;
             }
-//                        txtarea.setText(name);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        if (issuccess){
-            Intent goStart = new Intent(Login.this,StartGame.class);
-            startActivity(goStart);
-        }
-        else {
-            Intent goregis = new Intent(Login.this,regisAc.class);
-            startActivity(goregis);
-        }
+//        for(int j =0 ; j < lang ;j++) {
+//            if (user[j].equals(userName)) ch = true;
+//        }
+        return ch;
     }
+
     public String readJSONFromAsset() {
         String json = null;
         try {
