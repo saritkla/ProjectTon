86f7e7adfc70e5303c6238e1d22db116b23fa459
59050316 <sarit.28381@gmail.com>
2020-02-24 19:20:30 +0700

    kladone immage bt 24/2/20

--- app/src/main/java/com/example/goodreader/StartGame.java
+++ app/src/main/java/com/example/goodreader/StartGame.java
@@ -2,14 +2,35 @@ package com.example.goodreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StartGame extends AppCompatActivity {
+
+    ImageButton readfast,rooread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startgame);
+        readfast = (ImageButton)findViewById(R.id.fastread);
+        rooread = (ImageButton)findViewById(R.id.rooread);
+        readfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goqq =new Intent(StartGame.this,Login.class);
+                startActivity(goqq);
+            }
+        });
+        rooread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gokkk = new Intent(StartGame.this,regisAc.class);
+                startActivity(gokkk);
+            }
+        });
 
     }
 }
