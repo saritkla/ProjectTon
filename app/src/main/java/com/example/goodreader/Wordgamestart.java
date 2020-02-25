ec915217e5bf0533a669587335c1d19a9259c900
Tonton <mr.phuwamet@gmail.com>
2020-02-25 23:21:53 +0700

    TON

--- app/src/main/java/com/example/goodreader/Wordgamestart.java
+++ app/src/main/java/com/example/goodreader/Wordgamestart.java
@@ -13,10 +13,6 @@ public class Wordgamestart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(
-                Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_wordgamestart);
     }
 }
