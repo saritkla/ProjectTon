816582fbf1aa542fb6288a9108807de9dd6346df
mrphu <sarit.28381@gmail.com>
2020-03-05 23:19:11 +0700

    TON

--- app/src/main/java/com/example/goodreader/settingAc.java
+++ app/src/main/java/com/example/goodreader/settingAc.java
@@ -2,13 +2,25 @@ package com.example.goodreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class settingAc extends AppCompatActivity {
-
+    ImageButton logout,save ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingac);
+
+        logout = (ImageButton) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logout = new Intent(settingAc.this,Login.class);
+                startActivity(logout);
+            }
+        });
     }
 }
