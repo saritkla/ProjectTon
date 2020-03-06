4a1b0be2cef84db2a444081887f6e1ba8e909f93
mrphu <sarit.28381@gmail.com>
2020-03-06 21:58:09 +0700

    TTT

---
+++ app/src/main/java/com/example/goodreader/sumtraingame.java
@@ -0,0 +1,14 @@
+package com.example.goodreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sumtraingame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sumtraingame);
+    }
+}
