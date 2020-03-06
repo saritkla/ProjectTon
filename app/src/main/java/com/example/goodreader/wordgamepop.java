36503b1079d5e06b0849b103342746285fce6dd0
mrphu <sarit.28381@gmail.com>
2020-03-06 21:09:46 +0700

    TTT

--- app/src/main/java/com/example/goodreader/wordgamepop.java
+++ app/src/main/java/com/example/goodreader/wordgamepop.java
@@ -19,7 +19,7 @@ public class wordgamepop extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8.5) , (int)(height*.4));
+        getWindow().setLayout((int)(width*.8) , (int)(height*.6));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
