6313f4b795487b1593971a1d52563490de2a3380
kla <sarit.28381@gmail.com>
2020-04-28 23:18:45 +0700

    done word game 200 word limit done readding strie limit and to firebase

--- app/src/main/java/com/example/goodreader/traingame1.java
+++ app/src/main/java/com/example/goodreader/traingame1.java
@@ -41,6 +41,8 @@ public class traingame1 extends AppCompatActivity {
     int countword,countmain,wordID;
     long sumtime;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
